ed0c973b96ddfc2fe0db617200f37d272cc84e1e
Rahul Jain <148176604+Rahul2006MILLION@users.noreply.github.com>
2026-03-25 11:27:25 +0530

    Add files via upload
    # Two Sum (Java)
    
    ## Overview
    
    Simple Java program to find indices of two numbers in an array that add up to a target.
    
    ## Example
    
    Input:
    nums = [2, 7, 11, 15], target = 9
    Output:
    [0, 1]
    
    ## Approach
    
    * Uses brute-force (nested loops)
    * Time: O(n²), Space: O(1)
    
    ## Run
    
    ```bash
    javac Main.java
    java Main
    ```
    
    ## Output
    
    ```
    Indices: 0, 1
    ```

---
+++ Two_Sum.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Two_Sum {
+    static class Solution {
+        public int[] twoSum(int[] nums, int target) {
+            int[] arr = new int[2];
+
+            for (int i = 0; i < nums.length; i++) {
+                for (int j = i + 1; j < nums.length; j++) {
+                    if (nums[i] + nums[j] == target) {
+                        arr[0] = i;
+                        arr[1] = j;
+                        return arr;
+                    }
+                }
+            }
+            return arr;
+        }
+    }
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+
+        int[] result = sol.twoSum(nums, target);
+
+        System.out.println("Indices: " + result[0] + "," + result[1]);
+    }
+}
